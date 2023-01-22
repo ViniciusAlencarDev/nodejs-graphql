6fa033aeed10fb993f2b076c86ccb43c09809e52
Vinicius Alencar <viniciusalencardev@gmail.com>
2023-01-22 20:59:04 -0300

    Implementing graphl and express graphql library

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
-console.log('Init');
+// console.log('Init');
 
 export function getLengthMessage(msg: string): number {
     return msg.length;
 }
+
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { graphql, buildSchema } from 'graphql';
+
+const schema  = buildSchema(`
+    type Query {
+        hello: String
+    }
+`)
+
+const rootValue = {
+    hello: () => {
+        return 'Hello world!';
+    }
+}
+
+// graphql({
+//     schema,
+//     source: '{ hello }',
+//     rootValue
+//   }).then((response) => {
+//     console.log(response);
+//   });
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: rootValue,
+  graphiql: true,
+}));
+app.listen(3000);
+console.log('Started a GraphQL API server at http://localhost:3000/graphql');
